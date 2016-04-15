df952802957a543c58acef0ed7b35c3fc85a5742
Jivan517 <np.jiv004@gmail.com>
2016-04-15 15:03:55 -0500

    Dao added

--- code/src/main/java/cs544/project/onlineshoppingstore/dao/OrderDao.java
+++ code/src/main/java/cs544/project/onlineshoppingstore/dao/OrderDao.java
@@ -1,5 +1,16 @@
 package cs544.project.onlineshoppingstore.dao;
 
-public interface OrderDao {
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cs544.project.onlineshoppingstore.model.Order;
+import cs544.project.onlineshoppingstore.model.OrderStatus;
+
+public interface OrderDao extends JpaRepository<Order, Long>{
+
+	List<Order> findByOrderDate(Date date);
+	List<Order> findByOrderStatus(OrderStatus orderStatus);
+	
 }
