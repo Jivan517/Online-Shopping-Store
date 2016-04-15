2ad55874ec55349e2ba2f3f8cf3bee7b26895019
Jivan517 <np.jiv004@gmail.com>
2016-04-15 12:12:47 -0500

    shipping updated

--- code/src/main/java/cs544/project/onlineshoppingstore/model/Shipping.java
+++ code/src/main/java/cs544/project/onlineshoppingstore/model/Shipping.java
@@ -1,13 +1,11 @@
 package cs544.project.onlineshoppingstore.model;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
