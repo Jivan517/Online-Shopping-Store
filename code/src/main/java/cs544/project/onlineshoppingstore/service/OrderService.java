09cbe1cd923f51548c81187f044a39dcce0c2a1a
Jivan517 <np.jiv004@gmail.com>
2016-04-15 16:21:25 -0500

    service for order added

--- code/src/main/java/cs544/project/onlineshoppingstore/service/OrderService.java
+++ code/src/main/java/cs544/project/onlineshoppingstore/service/OrderService.java
@@ -1,5 +1,18 @@
 package cs544.project.onlineshoppingstore.service;
 
+import java.util.Date;
+import java.util.List;
+
+import cs544.project.onlineshoppingstore.model.Order;
+import cs544.project.onlineshoppingstore.model.OrderStatus;
+
 public interface OrderService {
 
+	
+	void create(Order order);
+	void update(long id, Order updatedOrder);
+	void delete(long orderId);
+	
+	List<Order> findByOrderDate(Date date);
+	List<Order> findByOrderStatus(OrderStatus orderStatus);
 }
--- code/src/main/java/cs544/project/onlineshoppingstore/service/OrderServiceImpl.java
+++ code/src/main/java/cs544/project/onlineshoppingstore/service/OrderServiceImpl.java
@@ -1,5 +1,51 @@
 package cs544.project.onlineshoppingstore.service;
 
-public class OrderServiceImpl {
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cs544.project.onlineshoppingstore.dao.OrderDao;
+import cs544.project.onlineshoppingstore.model.Order;
+import cs544.project.onlineshoppingstore.model.OrderStatus;
+
+public class OrderServiceImpl implements OrderService {
+
+	private OrderDao orderDao;
+	
+	@Autowired
+	public void setOrderDao(OrderDao orderDao) {
+		this.orderDao = orderDao;
+	}
+	
+	@Override
+	public void create(Order order) {
+		
+		orderDao.save(order);
+		
+	}
+
+	@Override
+	public void update(long id, Order updatedOrder) {
+		
+		orderDao.save(updatedOrder);
+		
+	}
+
+	@Override
+	public void delete(long orderId) {
+		orderDao.delete(orderId);
+		
+	}
+
+	@Override
+	public List<Order> findByOrderDate(Date date) {
+		return orderDao.findByOrderDate(date);
+	}
+
+	@Override
+	public List<Order> findByOrderStatus(OrderStatus orderStatus) {
+		return orderDao.findByOrderStatus(orderStatus);
+	}
 
 }
