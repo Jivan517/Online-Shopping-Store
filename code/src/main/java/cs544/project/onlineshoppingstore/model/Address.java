e9103aefdf189a9f049335bec128654099f16b51
AmitNiroula <nirol.amit@gmail.com>
2016-04-18 11:20:13 -0500

    add customer is fine

--- code/src/main/java/cs544/project/onlineshoppingstore/model/Address.java
+++ code/src/main/java/cs544/project/onlineshoppingstore/model/Address.java
@@ -22,5 +22,55 @@ public abstract class Address {
 	private String zip;
 	
 	private String street;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	
 	
 }
