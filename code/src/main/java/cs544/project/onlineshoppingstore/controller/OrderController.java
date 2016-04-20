69d4026069b6196c5c935b51bec25a0a05acab7a
AmitNiroula <nirol.amit@gmail.com>
2016-04-19 22:24:04 -0500

    order history

--- code/src/main/java/cs544/project/onlineshoppingstore/controller/OrderController.java
+++ code/src/main/java/cs544/project/onlineshoppingstore/controller/OrderController.java
@@ -2,12 +2,9 @@ package cs544.project.onlineshoppingstore.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,13 +24,17 @@ public class OrderController {
 	private CustomerService customerService;
 
 	
-	@RequestMapping(value = "/orderHistory/{id}", method = RequestMethod.GET)
-	public String update(@PathVariable int id, Model model){
+	@RequestMapping(value = "/order/orderhistory/{id}", method = RequestMethod.GET)
+	public String viewOrderHistory(@PathVariable int id, Model model){
 		
 		Customer customer = customerService.get(id);
-		List<Order> order = customer.getOrder();
-		model.addAttribute("order", order);
-		
-		return "customer/orderHistory";
+		List<Order> orders = customer.getOrder();
+		for(Order o : orders){
+			System.out.println(o.getDelivaryDate());
+			o.getCustomer().getName();
+		}
+		model.addAttribute("orders", orders);
+	
+		return "order/orderHistory";
 	}
 }
